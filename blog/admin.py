a5cb67300638dd0a4c976b14954aa98e17944e78
Even <evenhb@163.com>
2018-04-12 22:30:54 +0800

    Blog files +1

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post, Tag, Category
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modifiel_time', 'category', 'author']
+# Register your models here.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Category)
